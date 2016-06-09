62bc40aac2e080aceeb27d5d583ac46b8c353437
Carlos Andrés Cardona M <candres0819@gmail.com>
2016-06-09 00:55:38 -0500

    Signed-off-by: Carlos Andrés Cardona M <candres0819@gmail.com>

--- src/main/java/com/carloscardona/icroservices/configserver/ConfigServerApplication.java
+++ src/main/java/com/carloscardona/icroservices/configserver/ConfigServerApplication.java
@@ -5,6 +5,18 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * The Main Spring Boot Application class.<br>
+ * <br>
+ * 
+ * The {@link EnableConfigServer} annotation defines that this application will serve as the REST based API for providing external
+ * configuration. <br>
+ * <br>
+ * 
+ * The external repository from where the configuration will be picked up is defined in the {@linkplain application.yml} file.
+ *
+ * @author candr
+ */
 @EnableAutoConfiguration
 @EnableConfigServer
 @ComponentScan
